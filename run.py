da84be83310323ba8692695d440ea128116dedb2
Nigel Doheny <nigel.doheny93@gmail.com>
2022-06-14 11:42:57 +0000

    Add budget function and basic questions

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+user = input("Type in your name and press return: ")
+#try and expect here incase they write numbers in there name
+available = float(input("Type in the amount of money you have and press return: "))
+#try and expect here incase they write letters  
+print(f"Your name is {user} and the amount you have is {available}")
+confirm_user = input("Is this correct? Please type y or n: ")
+#try and expect or if to continue or change the name or amount
+
+budget = {}
+expenditure = {}
+
+def add_budget(name,amount):
+    global available
+    budget[name] = amount
+    available -= amount
+    expenditure[name] = 0
+    return available
+
