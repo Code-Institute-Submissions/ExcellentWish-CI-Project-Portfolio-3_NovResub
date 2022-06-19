ebb30c724b835118b1f3de607a85521d88c554a5
Nigel Doheny <nigel.doheny93@gmail.com>
2022-06-19 12:39:54 +0000

    Add budget.py and add_budget function and spend function

---
+++ budget.py
@@ -0,0 +1,43 @@
+from income_broken_down import week_income
+
+available = week_income
+budgets = {}
+expenditure = {}
+
+def add_budget(name , amount):
+    global available
+    if name in budgets:
+        raise ValueError("Budget exists")
+    if amount > available:
+        raise ValueError("Insufficient funds")    
+    budgets[name] = amount
+    available -= amount
+    expenditure[name] = 0
+    return print(budgets,available)
+
+def spend(name,amount):
+    """
+    Notes the amount of money you spent and the name of the budget you want to keep track it against
+    """
+    if name not in expenditure:
+        raise ValueError("No such budget")
+    expenditure[name] += amount
+    budgeted = budgets[name]
+    spent = expenditure[name]
+    
+    remaining_from_budget = budgeted - spent
+    return print(f"{remaining_from_budget} remaining from budget")    
+
+def print_summary():
+    for name in budgets:
+        budgeted = budgets[name]
+        spent = expenditure[name]
+        remaining = budgeted - spent
+        left_from_spent = remaining + available
+        print(name, budgeted , spent, remaining, left_from_spent)
+    
+
+add_budget("food", 100)
+spend("food",90)
+
+print_summary()
--- run.py
+++ run.py
@@ -5,37 +5,3 @@ available = float(input("Type in the amount of money you have and press return: 
 print(f"Your name is {user} and the amount you have is {available}")
 confirm_user = input("Is this correct? Please type y or n: ")
 #try and expect or if to continue or change the name or amount
-
-budget = {}
-expenditure = {}
-
-def add_budget(name,amount):
-    """
-    Takes the name of the budget and the amount of money to be budgeted
-    """
-    global available
-    if name in budget:
-        raise ValueError("Budget name exists")
-    if amount > available:
-        raise ValueError("Insufficient funds")
-    budget[name] = amount
-    available -= amount
-    expenditure[name] = 0
-    return available
-
-name = input("What is this budget for?")
-amount = float(input("How much can you afford for this?"))
-
-add_budget(name,amount)
-print(f"the amount left {add_budget(name,amount)}")
-
-def spend(name,amount):
-    """
-    Notes the amount of money you spent and the name of the budget you want to keep track it against
-    """
-    if name not in expenditure:
-        raise ValueError("No such budget")
-    expenditure[name] += amount
-    budgeted = budget[name]
-    spent = expenditure[name]
-    return budgeted - spent    
