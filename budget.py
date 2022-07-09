d92ee1406a423e8c0bede28970492e203fb5b0ef
Nigel Doheny <nigel.doheny93@gmail.com>
2022-07-09 09:56:52 +0000

    Function user input budget

--- budget.py
+++ budget.py
@@ -1,8 +1,20 @@
 from income_broken_down import week_income
+from prettytable import PrettyTable
+from colorama import init
+from colorama import Fore
+init()
 
 available = week_income
 budgets = {}
 expenditure = {}
+
+def userInputBudget():
+    print(Fore.WHITE)
+    budgetname = str(input('Enter the budget name: '))
+    budgetAmount = float(input('Enter the amount you have set aside for the budget: '))
+    spendings = float(input('Enter the amount you have spent: '))
+    add_budget(budgetname, budgetAmount)
+    spend(budgetname, spendings)
 
 def add_budget(name , amount):
     global available
@@ -35,11 +47,18 @@ def print_summary():
         remaining = budgeted - spent
         global left_from_income
         left_from_income = remaining + available
-        print(name, budgeted, spent, remaining, left_from_income)
+
+        # use pretty tables for print summary
+        x = PrettyTable()
+        x.field_names = ["Budget Name", "Budgeted Amount", "Spent", "Remainder from Budget", "Left from Income"]
+        x.add_row([name, budgeted, spent, remaining, left_from_income])
+        print(x)
+        # print('This is your budget summary:')
+        # print('Name | Budgeted | Spent | Remaining | Left From Income')
+        # print(name, budgeted, spent, remaining, left_from_income)
     
 
-
-add_budget("food", 100)
-spend("food",90)
-
+print(Fore.WHITE)
+userInputBudget()
+print(Fore.CYAN)
 print_summary()
