e8acb633debe14c453539b322a9138b21a325f42
Nigel Doheny <nigel.doheny93@gmail.com>
2022-07-13 17:31:33 +0000

    Add heading function for python budget calculator

--- run.py
+++ run.py
@@ -3,19 +3,14 @@ import income_broken_down
 import budget
 import interest_calculator
 
-import art
-
 
 
 def main():
+    heading()
     confirm_user
     income_broken_down
     budget
     interest_calculator
 
-
-Art = text2art("Budget Calculator")
-
-print(Art)
-
 main()
+
