df686c6065b773a1f3d1882f5254e48b31bd8056
Nigel Doheny <nigel.doheny93@gmail.com>
2022-07-17 09:43:41 +0000

    Fix confirm_user.py according to PEP8

--- confirm_user.py
+++ confirm_user.py
@@ -1,43 +1,49 @@
+from prettytable import PrettyTable
 from colorama import init
 from colorama import Fore
 init()
-from prettytable import PrettyTable
 
 
 def heading():
-    #Heading for app  
+    # Heading for app
     x = PrettyTable()
     x.field_names = ["Budget Calculator"]
     x.add_row(["This will calculate your Income"])
     x.add_row(["This will also create your budgets"])
-    x.add_row(["And also show you how much you can invest to become a millionaire"])
+    x.add_row(["And also show you how much you can invest"])
+    x.add_row(["And how long to become a millionaire"])
     print(x)
 
 heading()
 
 user = input("Type in your name and press return:\n ")
 available = float(input(Fore.GREEN + "What is your weekly income: "))
-print(Fore.BLUE + f"Your name is {user} and the amount you have is {available}" + Fore.WHITE)
+print(Fore.BLUE)
+print(f"Your name is {user} and your income is {available}")
+print(Fore.WHITE)
 confirm_user = input("Is this correct? Please type y or n:0 \n")
 """
-This will validate the users input 
+This will validate the users input
 """
-        
-if confirm_user != "y": 
+
+if confirm_user != "y":
     while (confirm_user == "n", confirm_user != "y" and confirm_user != "n"):
         if confirm_user != "y" and confirm_user != "n":
             print(Fore.RED + "Not a valid charater, please enter y or n.\n")
-            print(Fore.BLUE + f"Your name is {user} and the amount you have is {available}" + Fore.WHITE)
-            confirm_user = input("Is this correct? Please type y or n:1 ") 
-            
+            print(Fore.BLUE)
+            print(f"Your name is {user} and your income is {available}")
+            print(Fore.WHITE)
+            confirm_user = input("Is this correct? Please type y or n:\n")
             if confirm_user == "y":
                 break
-        
         user = input("Type in your name and press return:\n ")
-        available = float(input("Type in the amount of money you have and press return: \n"))
-        print(Fore.BLUE + f"Your name is {user} and the amount you have is {available}" + Fore.WHITE)
-        confirm_user = input("Is this correct? Please type y or n:2 ") 
+        available = float(input("Enter your weekly income:\n"))
+        print(Fore.BLUE)
+        print(f"Your name is {user} and your income is{available}")
+        print(Fore.WHITE)
+        confirm_user = input("Is this correct? Please type y or n:\n")
         if confirm_user == "y":
             break
-
-print(Fore.BLUE + f"End of user confirmation user: {user} money: {available}\n" + Fore.WHITE)
+print(Fore.BLUE)
+print(f"End of user confirmation user: {user} money: {available}\n")
+print(Fore.WHITE)
