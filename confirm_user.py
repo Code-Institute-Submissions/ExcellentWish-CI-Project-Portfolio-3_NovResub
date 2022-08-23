63122f0916c168ddb6283862375958be0a3c77e1
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-23 10:54:10 +0000

    Add error exceptions to confirm_user.py

--- confirm_user.py
+++ confirm_user.py
@@ -16,12 +16,41 @@ def heading():
 
 heading()
 
-user = input("Type in your name and press return:\n ")
-available = float(input(Fore.GREEN + "What is your weekly income: "))
+while True:
+    user = input("Type in your name and press return:\n ")
+    try:
+       if user == "":
+        raise ValueError('empty string')  
+    except ValueError as e:
+        print(e)
+        print('Valid name, please')
+        continue
+    if user == str(user):
+       break
+    else:
+       print('Valid Thank you')               
+
+while True:
+    try:
+        available = float(input(Fore.GREEN + "What is your weekly income: "))
+        if available == "":
+            raise ValueError('empty Value')
+    except ValueError as e:
+        print(e)
+        print('Valid value, please')
+        continue
+    if available == float():
+       break
+    else:
+       print('Valid Thank you') 
+       break
+
+
 print(Fore.BLUE)
 print(f"Your name is {user} and your income is {available}")
 print(Fore.WHITE)
 confirm_user = input("Is this correct? Please type y or n: \n")
+
 """
 This will validate the users input
 """
